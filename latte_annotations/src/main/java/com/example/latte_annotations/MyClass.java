882b9566581c65dec3a98ea7de8d1101a8c45a93
gao_haohao@126.com <gao_haohao@126.com>
2018-12-11 23:15:12 +0800

    java 库中添加注解

--- latte_annotations/src/main/java/com/example/latte_annotations/MyClass.java
+++ latte_annotations/src/main/java/com/example/latte_annotations/MyClass.java
@@ -1,4 +1,6 @@
 package com.example.latte_annotations;
-
+/*java 库
+* 注解库
+* */
 public class MyClass {
 }
--- latte_compiler/src/main/java/com/example/latte_compiler/MyClass.java
+++ latte_compiler/src/main/java/com/example/latte_compiler/MyClass.java
@@ -1,4 +1,7 @@
 package com.example.latte_compiler;
-
+/*
+* java 库
+* 代码生成库
+* */
 public class MyClass {
 }
