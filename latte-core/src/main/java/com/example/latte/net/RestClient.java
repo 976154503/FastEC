c2cd09a1b80c28e35314b536c46753d1f6ecfc48
gao_haohao@126.com <gao_haohao@126.com>
2018-12-18 23:41:47 +0800

    初始化RestClient

--- latte-core/src/main/java/com/example/latte/net/RestClient.java
+++ latte-core/src/main/java/com/example/latte/net/RestClient.java
@@ -1,6 +1,42 @@
 package com.example.latte.net;
+
+import com.example.latte.net.callback.IError;
+import com.example.latte.net.callback.IFailure;
+import com.example.latte.net.callback.IRequest;
+import com.example.latte.net.callback.ISuccess;
+
+import java.util.WeakHashMap;
+
+import okhttp3.RequestBody;
+
 /*
 * 网络请求具体实现类
 * */
-public class RestClient {
+public final class RestClient {
+    private final WeakHashMap<String, Object> PARAMS;
+    private final String URL;
+    private final IRequest REQUEST;
+    private final ISuccess SUCCESS;
+    private final IFailure FAILURE;
+    private final IError ERROR;
+    private final RequestBody BODY;
+
+    RestClient(String url,
+               WeakHashMap<String, Object> params,
+               IRequest request,
+               ISuccess success,
+               IFailure failure,
+               IError error,
+               RequestBody body) {
+        this.URL = url;
+        this.PARAMS = params;
+        this.REQUEST = request;
+        this.SUCCESS = success;
+        this.FAILURE = failure;
+        this.ERROR = error;
+        this.BODY = body;
+        }
+    public static RestClientBuilder builder() {
+        return new RestClientBuilder();
+    }
 }
---
+++ latte-core/src/main/java/com/example/latte/net/RestClientBuilder.java
@@ -0,0 +1,71 @@
+package com.example.latte.net;
+
+import com.example.latte.net.callback.IError;
+import com.example.latte.net.callback.IFailure;
+import com.example.latte.net.callback.IRequest;
+import com.example.latte.net.callback.ISuccess;
+
+import java.util.WeakHashMap;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+
+public final class RestClientBuilder {
+
+    private final WeakHashMap<String, Object> PARAMS = new WeakHashMap<>();
+    private String mUrl = null;
+    private IRequest mIRequest = null;
+    private ISuccess mISuccess = null;
+    private IFailure mIFailure = null;
+    private IError mIError = null;
+    private RequestBody mBody = null;
+
+    RestClientBuilder() {
+    }
+
+    public final RestClientBuilder url(String url) {
+        this.mUrl = url;
+        return this;
+    }
+
+    public final RestClientBuilder params(WeakHashMap<String, Object> params) {
+        PARAMS.putAll(params);
+        return this;
+    }
+
+    public final RestClientBuilder params(String key, Object value) {
+        PARAMS.put(key, value);
+        return this;
+    }
+
+    public final RestClientBuilder raw(String raw) {
+        this.mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raw);
+        return this;
+    }
+
+    public final RestClientBuilder onRequest(IRequest iRequest) {
+        this.mIRequest = iRequest;
+        return this;
+    }
+
+    public final RestClientBuilder success(ISuccess iSuccess) {
+        this.mISuccess = iSuccess;
+        return this;
+    }
+
+    public final RestClientBuilder failure(IFailure iFailure) {
+        this.mIFailure = iFailure;
+        return this;
+    }
+
+    public final RestClientBuilder error(IError iError) {
+        this.mIError = iError;
+        return this;
+    }
+
+    public final RestClient build() {
+        return new RestClient(mUrl, PARAMS,
+                mIRequest, mISuccess, mIFailure,
+                mIError, mBody);
+    }
+}
